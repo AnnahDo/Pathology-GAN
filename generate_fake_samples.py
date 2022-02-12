3f850ec1e88defe42e74d131790cf482670d7aa5
AnnahDo <annah.katharina@gmail.com>
2022-02-12 18:13:54 +0100

    removed necessity for dataset when generating images

--- generate_fake_samples.py
+++ generate_fake_samples.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
-import os 
+import os
 import argparse
 from data_manipulation.data import Data
 from models.generative.gans.PathologyGAN import PathologyGAN
 from models.evaluation.features import *
 
-
 parser = argparse.ArgumentParser(description='PathologyGAN fake image generator.')
-parser.add_argument('--checkpoint', dest='checkpoint', required=True, help='Path to pre-trained weights (.ckt) of PathologyGAN.')
-parser.add_argument('--num_samples', dest='num_samples', required=False, type=int, default=5000, help='Number of images to generate.')
+parser.add_argument('--checkpoint', dest='checkpoint', required=True,
+                    help='Path to pre-trained weights (.ckt) of PathologyGAN.')
+parser.add_argument('--num_samples', dest='num_samples', required=False, type=int, default=5000,
+                    help='Number of images to generate.')
 parser.add_argument('--batch_size', dest='batch_size', required=False, type=int, default=50, help='Batch size.')
 parser.add_argument('--z_dim', dest='z_dim', required=True, type=int, default=200, help='Latent space size.')
 parser.add_argument('--dataset', dest='dataset', type=str, default='vgh_nki', help='Dataset to use.')
@@ -30,9 +31,9 @@ dbs_path = args.dbs_path
 model = args.model
 
 if main_path is None:
-	main_path = os.path.dirname(os.path.realpath(__file__))
+    main_path = os.path.dirname(os.path.realpath(__file__))
 if dbs_path is None:
-	dbs_path = os.path.dirname(os.path.realpath(__file__))
+    dbs_path = os.path.dirname(os.path.realpath(__file__))
 
 # Dataset information.
 data_out_path = os.path.join(main_path, 'data_model_output')
@@ -59,12 +60,18 @@ gp_coeff = .65
 use_bn = False
 loss_type = 'relativistic gradient penalty'
 
-data = Data(dataset=dataset, marker=marker, patch_h=image_height, patch_w=image_width, n_channels=image_channels, batch_size=batch_size, project_path=dbs_path)
+data = Data(dataset=dataset, marker=marker, patch_h=image_height, patch_w=image_width, n_channels=image_channels,
+            batch_size=batch_size, project_path=dbs_path)
 
 with tf.Graph().as_default():
-    pathgan = PathologyGAN(data=data, z_dim=z_dim, layers=layers, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2, n_critic=n_critic, gp_coeff=gp_coeff, loss_type=loss_type, model_name='PathologyGAN')
-    gen_hdf5_path = generate_samples_from_checkpoint(model=pathgan, data=data, data_out_path=main_path, checkpoint=checkpoint, num_samples=num_samples, batches=batch_size)
+    pathgan = PathologyGAN(data=data, z_dim=z_dim, layers=layers, use_bn=use_bn, alpha=alpha, beta_1=beta_1,
+                           learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2,
+                           n_critic=n_critic, gp_coeff=gp_coeff, loss_type=loss_type, model_name='PathologyGAN')
+    gen_hdf5_path = generate_samples_from_checkpoint(model=pathgan, data=data, data_out_path=main_path,
+                                                     checkpoint=checkpoint, num_samples=num_samples, batches=batch_size)
 
 # Generate Inception features from fake images.
 with tf.Graph().as_default():
-    hdf5s_features = inception_tf_feature_activations(hdf5s=[gen_hdf5_path], input_shape=data.training.shape[1:], batch_size=batch_size)
+    hdf5s_features = inception_tf_feature_activations(hdf5s=[gen_hdf5_path],
+                                                      input_shape=[data.patch_h, data.patch_w, data.n_channels],
+                                                      batch_size=batch_size)
